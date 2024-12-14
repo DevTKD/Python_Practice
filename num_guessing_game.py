30aae9238ab0d8f200bed31055ce2bcae05f201c
Tanya Davis <tdavis_la@outlook.com>
2024-12-14 16:43:23 -0500

    inital commit

---
+++ num_guessing_game.py
@@ -0,0 +1,6 @@
+"""
+🌟 Number Guessing Game
+A program that lets the user guess a randomly generated number between 1 and 100.
+"""
+user_number = int(input ("Guess a number between 1 and 100: "))
+
