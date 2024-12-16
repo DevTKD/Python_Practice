c41b09f6564abe308fc373a182104c6a83effdcd
Tanya Davis <tdavis_la@outlook.com>
2024-12-15 20:28:18 -0500

    Started age calculator

---
+++ age_calculator.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+from dateutil.relativedelta import relativedelta as rd
+
+"""
+📅 Age Calculator
+Input your birthdate and calculate your age in years, months, and days.
+"""
