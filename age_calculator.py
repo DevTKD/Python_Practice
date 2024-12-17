485f29f10ffcdc8938a083f9f6d9f7e7c48744a5
Tanya Davis <tdavis_la@outlook.com>
2024-12-16 20:34:06 -0500

    Added age calculator file

--- age_calculator.py
+++ age_calculator.py
@@ -1,7 +1,20 @@
 from datetime import datetime
-from dateutil.relativedelta import relativedelta as rd
 
 """
 📅 Age Calculator
 Input your birthdate and calculate your age in years, months, and days.
 """
+current_date = datetime.now()
+birthday_input = input("Enter your birthdate (YYYY-MM-DD): ")
+birthday = datetime.strptime(birthday_input, "%Y-%m-%d")
+
+# Calculate age
+age = current_date.year - birthday.year
+ageInMonths = current_date.month - birthday.month
+ageInDays = current_date.day - birthday.day
+
+print(f"Your exact ags is {age} years,  {ageInMonths} month(s), "
+      f"and {ageInDays} day(s) old! You look good! 😉.")
+
+
+
