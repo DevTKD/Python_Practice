196a41d55f78ccff437cc6fb43788ccf7d5bf223
Tanya Davis <tdavis_la@outlook.com>
2024-12-19 18:58:15 -0500

    Added program functionality

--- word_counter.py
+++ word_counter.py
@@ -1,4 +1,11 @@
 """
-📚Word Counter
+📚Simple Word Counter
 Enter a sentence, and the program counts the number of words and characters.
 """
+user_input = input("Enter a word or phase: ")
+
+word_count = len(user_input.split())
+character_count = len(user_input)
+
+print(f"Your word count is: {word_count}")
+print(f"Your character count is: {character_count}")
