e3407bf6a71c300f44cc329e593bc8026a91d801
Tanya Davis <tdavis_la@outlook.com>
2024-12-22 18:19:49 -0500

    add currency exchange rates and calculations

--- currency_converter.py
+++ currency_converter.py
@@ -1,9 +1,24 @@
 """
-💵 Currency Converter
+💵 Currency Converter (Version 1)
 Convert an amount from one currency to another using a predefined exchange rate.
 """
+# Dictionary to store exchange rates
+exchange_rates = {
+    "EUR": 0.96,
+    "USD": 1.0,
+    "GBP": 0.82,
+    "JPY": 110.0,
+    "CAD": 1.44
+}
 
 currency_amount = float(input("Enter currency amount: "))
-original_currency = input("Enter original currency: ")
-wanted_currency = input("Enter wanted currency: ")
+print("Currency Options: EUR, USD, GBP, JPY or CAD")
+from_currency = input("Enter original currency: ")
+target_currency = input("Enter wanted currency: ")
 
+# Check if the target currency is in the dictionary
+if target_currency in exchange_rates:
+    calculated_exchange_currency = currency_amount * exchange_rates[target_currency]
+    print(f"Your currency exchange is {calculated_exchange_currency} {target_currency}")
+else:
+    print("Enter a valid currency.")
