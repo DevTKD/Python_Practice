ba8aa98722451f6bce02c044933aad317c5cdc8e
Tanya Davis <tdavis_la@outlook.com>
2024-12-21 21:48:16 -0500

    Initial commit

---
+++ currency_converter.py
@@ -0,0 +1,9 @@
+"""
+💵 Currency Converter
+Convert an amount from one currency to another using a predefined exchange rate.
+"""
+
+currency_amount = float(input("Enter currency amount: "))
+original_currency = input("Enter original currency: ")
+wanted_currency = input("Enter wanted currency: ")
+
