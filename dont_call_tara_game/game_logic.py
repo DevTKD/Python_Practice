03b5403b7348688c41b9b5e69c6aab6171c869a8
Tanya Davis <tdavis_la@outlook.com>
2025-01-02 19:11:15 -0500

    added texted based game project

---
+++ dont_call_tara_game/game_logic.py
@@ -0,0 +1,7 @@
+"""
+🎮Dont Call Tara!  is an interactive text-based mystery game where players unravel a
+cybercrime entangled with murder. Your choices determine the outcome—will you uncover the truth or fall
+victim to deception?
+"""
+print("Welcome to Dont Call Tara! 📵")
+
