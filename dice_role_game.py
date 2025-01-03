1066dd02dccabdc99cf43ce2c9554b1f74b7951c
Tanya Davis <tdavis_la@outlook.com>
2025-01-03 17:29:20 -0500

    added dice roll simulator

---
+++ dice_role_game.py
@@ -0,0 +1,9 @@
+import random
+"""
+🎲 Simple Dice Roller Simulator
+Simulate rolling one or two dice and display the result.
+"""
+die_1 = random.randint(1, 6)
+die_2 = random.randint(1, 6)
+
+print(f"You rolled a {die_1} and a {die_2}")
