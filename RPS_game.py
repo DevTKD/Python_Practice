5d87824c7115f2b29ad88ff33ed9864111d4069b
Tanya Davis <tdavis_la@outlook.com>
2025-01-06 18:08:07 -0500

    Added RPS game stater code base

---
+++ RPS_game.py
@@ -0,0 +1,13 @@
+import random
+"""
+🪨, 📄, ✂️Rock-Paper-Scissors Game
+- Play a game of rock-paper-scissors against the computer.
+"""
+player_choices = ["Rock 🪨", "Paper 📄", "Scissors ✂️" ]
+
+player_1 = input("Select Rock, Paper or Scissors: ")
+computer_player = random.choice(player_choices)
+
+# need to add while loop for contentious game play
+# add if/else logic for game rules
+
