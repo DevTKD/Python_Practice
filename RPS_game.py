58e7f5fcd275cd430d957a3d462545605b4d2a81
Tanya Davis <tdavis_la@outlook.com>
2025-01-09 21:12:11 -0500

    Started game logic for RPS

--- RPS_game.py
+++ RPS_game.py
@@ -5,9 +5,19 @@ import random
 """
 player_choices = ["Rock 🪨", "Paper 📄", "Scissors ✂️" ]
 
-player_1 = input("Select Rock, Paper or Scissors: ")
-computer_player = random.choice(player_choices)
+player_1 = input("Select Rock, Paper or Scissors: ").capitalize()
+computer_player = random.choice(player_choices).capitalize()
 
 # need to add while loop for contentious game play
 # add if/else logic for game rules
+"""
+Game Rules:
+* Rock beats scissors:Rock blunts scissors
+* Scissors beats paper:Scissors cuts paper
+* Paper beats rock: Paper covers rock
+* Draw: If both players choose the same shape, the turn is a draw and repeated
+"""
+
+if player_1 == "Rock" and computer_player == "Scissors":
+    print("Rock 🪨 beats scissors ✂️:Rock blunts scissors")
 
