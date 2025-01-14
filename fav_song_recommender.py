ce87177656f4e037faf4e7b4afa1a4eb22d1592f
Tanya Davis <tdavis_la@outlook.com>
2025-01-13 19:04:59 -0500

    Started application for favorite song recommender

---
+++ fav_song_recommender.py
@@ -0,0 +1,48 @@
+""""
+🎶 Favorite Song Recommender
+Ask the user for their mood or genre preference and recommend a song
+"""
+
+user_input = input("What is your mood (happy, sad or mad) or favorite music genre (classical, Soul, Jazz, or Rock)?: ")
+
+sad_music = [
+    "Fix You by Coldplay",
+    "Someone Like You by Adele",
+    "Let Her Go by Passenger",
+    "The Night We Met by Lord Huron",
+    "I Will Always Love You by Whitney Houston",
+    "Skinny Love by Bon Iver"
+]
+
+happy_music = [
+    "Happy by Pharrell Williams",
+    "Can’t Stop the Feeling! by Justin Timberlake",
+    "Walking on Sunshine by Katrina and The Waves",
+    "Uptown Funk by Mark Ronson ft. Bruno Mars",
+    "Good as Hell by Lizzo",
+    "Dancing Queen by ABBA"
+]
+
+mad_music = [
+    "Smells Like Teen Spirit by Nirvana",
+    "Break Stuff by Limp Bizkit",
+    "You Oughta Know by Alanis Morissette",
+    "Killing in the Name by Rage Against the Machine"
+    "Before I Forget by Slipknot",
+    "Irreplaceable by Beyoncé"
+]
+
+classical_music = [
+    "Adagio for Strings by Samuel Barber",
+    "Moonlight Sonata (1st Movement) by Ludwig van Beethoven",
+    "Spring by Antonio Vivaldi",
+    "Ode to Joy by Beethoven",
+    "Dies Irae by Mozart",
+    "In the Hall of the Mountain King"
+]
+
+soul_music = []
+
+jazz_music = []
+
+rock_music = []
